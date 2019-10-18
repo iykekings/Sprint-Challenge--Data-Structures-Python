2121f32befd96948cb7cec90d094b2a6e3d71d8a
Ikechukwu <iykekings36@gmail.com>
2019-10-18 08:42:14 +0100

    remove manual test for ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,28 +5,10 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
-    # #  if capacity has been reached
-    # if(self.current + 1 == self.capacity):
-    #   # replace the item after current index in the storage
-    #   self.storage.insert(0, item)
-    #   #  reset self.current to 1
-    #   self.current = 1
-    # else:
-    #   self.storage.insert(self.current, item)
-    # # Increment self.current
-    # self.current += 1
+    #  Overwrites the oldest element in the storage
     self.storage[self.current % self.capacity] = item
+    # Increment current
     self.current += 1
 
   def get(self):
     return self.storage
-
-
-ike = RingBuffer(3)
-ike.append(2)
-ike.append(1)
-ike.append(4)
-ike.append(5)
-ike.append(7)
-ike.append(5)
-ike.append(6)
